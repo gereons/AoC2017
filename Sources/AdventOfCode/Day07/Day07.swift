26b928a6b84b5bc2711aaf72e0d16f0ccd44223d
Gereon Steffens <gereon@steffens.org>
2022-07-09 12:03:21 +0200

    day 7

--- Sources/AdventOfCode/Day07/Day07.swift
+++ Sources/AdventOfCode/Day07/Day07.swift
@@ -5,18 +5,117 @@
 //
 
 import AoCTools
+import Foundation
+
+private struct Program {
+    let name: String
+    let weight: Int
+    let carrying: [String]
+
+    static let regex = try! NSRegularExpression(pattern: #"(.*) \((\d*)\)( -> (.*))?"#)
+
+    init(_ string: String) {
+        let range = NSRange(location: 0, length: string.count)
+        let matches = Self.regex.matches(in: string, options: [], range: range)
+
+        let match = matches.last!
+        self.name = String(string[Range(match.range(at: 1), in: string)!])
+        self.weight = Int(String(string[Range(match.range(at: 2), in: string)!]))!
+        if let carryRange = Range(match.range(at: 4), in: string) {
+            let carry = String(string[carryRange])
+            self.carrying = carry.components(separatedBy: ", ")
+        } else {
+            self.carrying = []
+        }
+    }
+}
+
+extension Program: CustomStringConvertible {
+    var description: String {
+        return "\(name) (\(weight))"
+    }
+}
+
+private typealias Node = TreeNode<Program>
+
+extension Node {
+    var name: String { value.name }
+    var weight: Int { value.weight }
+    var treeWeight: Int { reduce(0) { $0 + $1.weight } }
+
+    func isUnbalancedAt() -> Int? {
+        let weights = children.map { $0.treeWeight }
+        if Set(weights).count > 1 {
+            let max = weights.max(by: <)
+            return weights.firstIndex { $0 == max }
+        } else {
+            return nil
+        }
+    }
+}
 
 final class Day07: AOCDay {
-    let input: String
+    private let programs: [Program]
+    private var root: Node?
+
     init(rawInput: String? = nil) {
-        self.input = rawInput ?? Self.rawInput
+        let input = rawInput ?? Self.rawInput
+        programs = input.components(separatedBy: "\n").map { Program($0) }
     }
 
-    func part1() -> Int {
-        return 0
+    func part1() -> String {
+        root = buildTree()
+        return root!.value.name
     }
 
     func part2() -> Int {
+        if root == nil { root = buildTree() }
+
+        var lastUnbalancedNode: Node?
+        root!.visitAll { node, level in
+            if let index = node.isUnbalancedAt() {
+                lastUnbalancedNode = node.children[index]
+            }
+        }
+
+        if let node = lastUnbalancedNode {
+            let weights = node.parent!.children.compactMap { $0.treeWeight }
+            let max = weights.max(by: <)!
+            let min = weights.min(by: <)!
+            return node.weight - (max - min)
+        }
         return 0
+    }
+
+    private func buildTree() -> Node {
+        var carriedBy = [String: Program]()
+        var byName = [String: Program]()
+        for program in programs {
+            byName[program.name] = program
+            for carried in program.carrying {
+                carriedBy[carried] = program
+            }
+        }
+
+        var rootProgram: Program?
+        for program in programs {
+            if carriedBy[program.name] == nil {
+                rootProgram = program
+                break
+            }
+        }
+
+        let root = createTree(at: rootProgram!, programs: byName)
+        return root
+    }
+
+    private func createTree(at root: Program, programs: [String: Program]) -> Node {
+        let node = Node(root)
+        for name in root.carrying {
+            let program = programs[name]!
+            let child = createTree(at: program, programs: programs)
+            node.add(child)
+        }
+        return node
     }
 }
--- Tests/aocTests/Day07Tests.swift
+++ Tests/aocTests/Day07Tests.swift
@@ -2,13 +2,29 @@ import XCTest
 @testable import AdventOfCode
 
 final class Day07Tests: XCTestCase {
+    let input = """
+pbga (66)
+xhth (57)
+ebii (61)
+havc (66)
+ktlj (57)
+fwft (72) -> ktlj, cntj, xhth
+qoyq (66)
+padx (45) -> pbga, havc, qoyq
+tknk (41) -> ugml, padx, fwft
+jptl (61)
+ugml (68) -> gyxo, ebii, jptl
+gyxo (61)
+cntj (57)
+"""
+
     func testDay07_1() throws {
-        let day = Day07(rawInput: "foo")
-        XCTAssertEqual(day.part1(), 0)
+        let day = Day07(rawInput: input)
+        XCTAssertEqual(day.part1(), "tknk")
     }
 
     func testDay07_2() throws {
-        let day = Day07(rawInput: "bar")
-        XCTAssertEqual(day.part2(), 0)
+        let day = Day07(rawInput: input)
+        XCTAssertEqual(day.part2(), 60)
     }
 }
