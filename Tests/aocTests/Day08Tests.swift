218b88f5d7188e29af5ff37dbb570b6b9f991ed4
Gereon Steffens <gereon@steffens.org>
2022-07-09 13:07:07 +0200

    day 8

--- Tests/aocTests/Day08Tests.swift
+++ Tests/aocTests/Day08Tests.swift
@@ -2,13 +2,20 @@ import XCTest
 @testable import AdventOfCode
 
 final class Day08Tests: XCTestCase {
+    let input = """
+    b inc 5 if a > 1
+    a inc 1 if b < 5
+    c dec -10 if a >= 1
+    c inc -20 if c == 10
+    """
+
     func testDay08_1() throws {
-        let day = Day08(rawInput: "foo")
-        XCTAssertEqual(day.part1(), 0)
+        let day = Day08(rawInput: input)
+        XCTAssertEqual(day.part1(), 1)
     }
 
     func testDay08_2() throws {
-        let day = Day08(rawInput: "bar")
-        XCTAssertEqual(day.part2(), 0)
+        let day = Day08(rawInput: input)
+        XCTAssertEqual(day.part2(), 10)
     }
 }
