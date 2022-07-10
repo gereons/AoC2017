590f975f57010257d485bdc4844ea23904acf60e
Gereon Steffens <gereon@steffens.org>
2022-07-10 19:36:46 +0200

    Day 9

--- Sources/AdventOfCode/Day09/Day09.swift
+++ Sources/AdventOfCode/Day09/Day09.swift
@@ -8,15 +8,54 @@ import AoCTools
 
 final class Day09: AOCDay {
     let input: String
+    private var result: (Int, Int)?
     init(rawInput: String? = nil) {
         self.input = rawInput ?? Self.rawInput
     }
 
+    typealias Node = TreeNode<Void>
+
     func part1() -> Int {
-        return 0
+        result = scoreGarbage()
+        return result!.0
     }
 
     func part2() -> Int {
-        return 0
+        if result == nil { result = scoreGarbage() }
+        return result!.1
+    }
+
+    private func scoreGarbage() -> (Int, Int) {
+        var groupScore = 0
+        var garbageCount = 0
+        var inGroup = 0
+        var inGarbage = false
+        var isEscaped = false
+        for ch in input {
+            if isEscaped {
+                isEscaped = false
+                continue
+            }
+            if inGarbage {
+                if ch == "!" { isEscaped = true; continue }
+                if ch != ">" { garbageCount += 1; continue }
+            }
+            switch ch {
+            case "{":
+                inGroup += 1
+            case "}":
+                groupScore += inGroup
+                inGroup -= 1
+            case "<":
+                inGarbage = true
+            case ">":
+                inGarbage = false
+            case "!":
+                isEscaped = true
+            default:
+                continue
+            }
+        }
+        return (groupScore, garbageCount)
     }
 }
--- Tests/aocTests/Day09Tests.swift
+++ Tests/aocTests/Day09Tests.swift
@@ -3,12 +3,24 @@ import XCTest
 
 final class Day09Tests: XCTestCase {
     func testDay09_1() throws {
-        let day = Day09(rawInput: "foo")
-        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(Day09(rawInput: "{}").part1(), 1)
+        XCTAssertEqual(Day09(rawInput: "{{{}}}").part1(), 6)
+        XCTAssertEqual(Day09(rawInput: "{{},{}}").part1(), 5)
+        XCTAssertEqual(Day09(rawInput: "{{{},{},{{}}}}").part1(), 16)
+        XCTAssertEqual(Day09(rawInput: "{<a>,<a>,<a>,<a>}").part1(), 1)
+        XCTAssertEqual(Day09(rawInput: "{{<ab>},{<ab>},{<ab>},{<ab>}}").part1(), 9)
+        XCTAssertEqual(Day09(rawInput: "{{<!!>},{<!!>},{<!!>},{<!!>}}").part1(), 9)
+        XCTAssertEqual(Day09(rawInput: "{{<a!>},{<a!>},{<a!>},{<ab>}}").part1(), 3)
+        XCTAssertEqual(Day09(rawInput: "{{<!>},{<!>},{<!>},{<a>}}").part1(), 3)
     }
 
     func testDay09_2() throws {
-        let day = Day09(rawInput: "bar")
-        XCTAssertEqual(day.part2(), 0)
+        XCTAssertEqual(Day09(rawInput: "<>").part2(), 0)
+        XCTAssertEqual(Day09(rawInput: "<random characters>").part2(), 17)
+        XCTAssertEqual(Day09(rawInput: "<<<<>").part2(), 3)
+        XCTAssertEqual(Day09(rawInput: "<{!>}>").part2(), 2)
+        XCTAssertEqual(Day09(rawInput: "<!!>").part2(), 0)
+        XCTAssertEqual(Day09(rawInput: "<!!!>>").part2(), 0)
+        XCTAssertEqual(Day09(rawInput: "<{o\"i!a,<{i<a>").part2(), 10)
     }
 }
