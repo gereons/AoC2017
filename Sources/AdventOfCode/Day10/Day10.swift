8dcc55aa7cb6aebb866ee65d48d68729d1c9c6e3
Gereon Steffens <gereon@steffens.org>
2022-07-11 11:33:47 +0200

    day 10 part 1

--- Sources/AdventOfCode/Day10/Day10.swift
+++ Sources/AdventOfCode/Day10/Day10.swift
@@ -6,17 +6,71 @@
 
 import AoCTools
 
+struct CircularBuffer {
+    let size: Int
+    var storage: [Int]
+
+    init(with data: [Int]) {
+        self.size = data.count
+        storage = data
+    }
+
+    private func realIndex(_ index: Int) -> Int {
+        index % size
+    }
+
+    func section(from start: Int, length: Int) -> [Int] {
+        var result = [Int]()
+        for idx in start..<start+length {
+            result.append(storage[realIndex(idx)])
+        }
+        assert(result.count == length)
+        return result
+    }
+
+    mutating func store(_ data: [Int], at start: Int) {
+        for (index, value) in data.enumerated() {
+            storage[realIndex(start + index)] = value
+        }
+    }
+}
+
 final class Day10: AOCDay {
-    let input: String
-    init(rawInput: String? = nil) {
-        self.input = rawInput ?? Self.rawInput
+    let input: [Int]
+    var buffer: CircularBuffer
+
+    convenience init(rawInput: String? = nil) {
+        self.init(rawInput: rawInput, list: nil)
+    }
+
+    init(rawInput: String? = nil, list: [Int]?) {
+        let input = rawInput ?? Self.rawInput
+        self.input = input.components(separatedBy: ",").compactMap { Int($0) }
+
+        if let list = list {
+            buffer = CircularBuffer(with: list)
+        } else {
+            buffer = CircularBuffer(with: [Int](0..<256))
+        }
     }
 
     func part1() -> Int {
-        return 0
+        var skip = 0
+        var position = 0
+
+        for length in input {
+            let seq = buffer.section(from: position, length: length).reversed()
+            buffer.store(Array(seq), at: position)
+
+            position += length + skip
+            skip += 1
+        }
+
+        let head = buffer.section(from: 0, length: 2)
+        return head.reduce(1, *)
     }
 
-    func part2() -> Int {
-        return 0
+    func part2() -> String {
+        return ""
     }
 }
--- Tests/aocTests/Day10Tests.swift
+++ Tests/aocTests/Day10Tests.swift
@@ -2,13 +2,34 @@ import XCTest
 @testable import AdventOfCode
 
 final class Day10Tests: XCTestCase {
+    func testBuffer() throws {
+        let cb = CircularBuffer(with: [0,1,2,3,4])
+
+        XCTAssertEqual(cb.section(from: 0, length: 2), [0,1])
+        XCTAssertEqual(cb.section(from: 3, length: 5), [3,4,0,1,2])
+    }
+    
+    func testBufferAssign() throws {
+        var cb = CircularBuffer(with: [0,1,2,3,4])
+
+        cb.store([9,8,7], at: 0)
+        XCTAssertEqual(cb.section(from: 0, length: 5), [9,8,7,3,4])
+
+        var cb2 = CircularBuffer(with: [0,1,2,3,4])
+
+        cb2.store([9,8,7], at: 4)
+        XCTAssertEqual(cb2.section(from: 0, length: 5), [8,7,2,3,9])
+    }
+
     func testDay10_1() throws {
-        let day = Day10(rawInput: "foo")
-        XCTAssertEqual(day.part1(), 0)
+        let day = Day10(rawInput: "3,4,1,5", list: [0,1,2,3,4])
+        XCTAssertEqual(day.part1(), 12)
     }
 
     func testDay10_2() throws {
-        let day = Day10(rawInput: "bar")
-        XCTAssertEqual(day.part2(), 0)
+        XCTAssertEqual(Day10(rawInput: "").part2(), "a2582a3a0e66e6e86e3812dcb672a272")
+        XCTAssertEqual(Day10(rawInput: "AoC 2017").part2(), "33efeb34ea91902bb2f59c9920caa6cd")
+        XCTAssertEqual(Day10(rawInput: "1,2,3").part2(), "3efbe78a8d82f29979031a4aa0b16a9d")
+        XCTAssertEqual(Day10(rawInput: "1,2,4").part2(), "63960835bcdc130f0b66d7ff4f6a5a8e")
     }
 }
