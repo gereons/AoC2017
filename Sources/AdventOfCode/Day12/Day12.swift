67d42a46c0b4fd78a7b4eb8f1033c4d5e0736675
Gereon Steffens <gereon@steffens.org>
2022-07-19 20:11:07 +0200

    day 12

--- Sources/AdventOfCode/Day12/Day12.swift
+++ Sources/AdventOfCode/Day12/Day12.swift
@@ -6,17 +6,58 @@
 
 import AoCTools
 
+private struct Program {
+    let id: Int
+    let connectedTo: Set<Int>
+
+    init(_ string: String) {
+        let parts = string.components(separatedBy: " <-> ")
+
+        self.id = Int(parts[0])!
+        self.connectedTo = Set(parts[1].components(separatedBy: ", ").map { Int($0)! })
+    }
+}
+
 final class Day12: AOCDay {
-    let input: String
+    private let programs: [Int: Program]
     init(rawInput: String? = nil) {
-        self.input = rawInput ?? Self.rawInput
+        let input = rawInput ?? Self.rawInput
+
+        let programs = input.components(separatedBy: "\n").map { Program($0) }
+        self.programs = Dictionary(uniqueKeysWithValues: zip(programs.map { $0.id }, programs))
     }
 
     func part1() -> Int {
-        return 0
+        let start = programs[0]!
+        var seen = Set<Int>()
+        findConnected(to: start, programs, &seen)
+        return seen.count
     }
 
     func part2() -> Int {
-        return 0
+        var programs = self.programs
+        var start = programs[0]
+        var seen = Set<Int>()
+
+        var groups = 0
+        while start != nil {
+            findConnected(to: start!, programs, &seen)
+            seen.forEach { programs[$0] = nil }
+            groups += 1
+            seen.removeAll()
+            start = programs.first?.value
+        }
+        return groups
+    }
+
+    private func findConnected(to program: Program, _ programs: [Int: Program], _ seen: inout Set<Int>) {
+        if seen.contains(program.id) {
+            return
+        }
+
+        seen.insert(program.id)
+        for connected in program.connectedTo {
+            findConnected(to: programs[connected]!, programs, &seen)
+        }
     }
 }
--- Tests/aocTests/Day12Tests.swift
+++ Tests/aocTests/Day12Tests.swift
@@ -2,13 +2,23 @@ import XCTest
 @testable import AdventOfCode
 
 final class Day12Tests: XCTestCase {
+    let test = """
+    0 <-> 2
+    1 <-> 1
+    2 <-> 0, 3, 4
+    3 <-> 2, 4
+    4 <-> 2, 3, 6
+    5 <-> 6
+    6 <-> 4, 5
+    """
+
     func testDay12_1() throws {
-        let day = Day12(rawInput: "foo")
-        XCTAssertEqual(day.part1(), 0)
+        let day = Day12(rawInput: self.test)
+        XCTAssertEqual(day.part1(), 6)
     }
 
     func testDay12_2() throws {
-        let day = Day12(rawInput: "bar")
-        XCTAssertEqual(day.part2(), 0)
+        let day = Day12(rawInput: self.test)
+        XCTAssertEqual(day.part2(), 2)
     }
 }
