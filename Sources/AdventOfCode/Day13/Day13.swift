3305decc1559606272400b050134928e942be3a5
Gereon Steffens <gereon@steffens.org>
2022-07-21 08:59:08 +0200

    day 13

--- Sources/AdventOfCode/Day13/Day13.swift
+++ Sources/AdventOfCode/Day13/Day13.swift
@@ -6,17 +6,73 @@
 
 import AoCTools
 
+class Firewall {
+    let depth: Int
+    let range: Int
+    private let scannerPositions: [Int]
+
+    var severity: Int { depth * range }
+
+    init(depth: Int, range: Int) {
+        self.depth = depth
+        self.range = range
+        self.scannerPositions = [Int](0..<range) + [Int](1..<range-1).reversed()
+    }
+
+    func scannerPosition(at tick: Int) -> Int {
+        return scannerPositions[tick % scannerPositions.count]
+    }
+
+    convenience init(_ string: String) {
+        let parts = string.components(separatedBy: ": ")
+        self.init(depth: Int(parts[0])!, range: Int(parts[1])!)
+    }
+}
+
 final class Day13: AOCDay {
-    let input: String
+    let firewalls: [Int: Firewall]
+    let maxDepth: Int
+
     init(rawInput: String? = nil) {
-        self.input = rawInput ?? Self.rawInput
+        let input = rawInput ?? Self.rawInput
+        let firewalls = input.components(separatedBy: "\n").map { Firewall($0) }
+
+        self.firewalls = Dictionary(uniqueKeysWithValues: zip(firewalls.map { $0.depth }, firewalls))
+        maxDepth = self.firewalls.keys.max(by: <)!
     }
 
     func part1() -> Int {
-        return 0
+        let severity = tripSeverity(delay: 0, stopWhenCaught: false)
+        return severity
+    }
+
+    private func tripSeverity(delay: Int, stopWhenCaught: Bool) -> Int {
+        var severity = 0
+        var packetDepth = -1
+        var picoSecond = -1
+        while packetDepth <= maxDepth {
+            picoSecond += 1
+            packetDepth += 1
+            if let fw = firewalls[packetDepth] {
+                if fw.scannerPosition(at: picoSecond + delay) == 0 {
+                    // print("caught at \(packetDepth) for \(fw.severity)")
+                    severity += fw.severity
+                    if stopWhenCaught {
+                        return 1
+                    }
+                }
+            }
+        }
+        return severity
     }
 
     func part2() -> Int {
-        return 0
+        for delay in 0 ... 10_000_000 {
+            let severity = tripSeverity(delay: delay, stopWhenCaught: true)
+            if severity == 0 {
+                return delay
+            }
+        }
+        return -1
     }
 }
--- Tests/aocTests/Day13Tests.swift
+++ Tests/aocTests/Day13Tests.swift
@@ -2,13 +2,20 @@ import XCTest
 @testable import AdventOfCode
 
 final class Day13Tests: XCTestCase {
+    let test = """
+    0: 3
+    1: 2
+    4: 4
+    6: 4
+    """
+
     func testDay13_1() throws {
-        let day = Day13(rawInput: "foo")
-        XCTAssertEqual(day.part1(), 0)
+        let day = Day13(rawInput: test)
+        XCTAssertEqual(day.part1(), 24)
     }
 
     func testDay13_2() throws {
-        let day = Day13(rawInput: "bar")
-        XCTAssertEqual(day.part2(), 0)
+        let day = Day13(rawInput: test)
+        XCTAssertEqual(day.part2(), 10)
     }
 }
