9a3d2047ff4fdd31c1aad664e468c83b0b3115ed
Gereon Steffens <gereon@steffens.org>
2022-07-23 10:53:09 +0200

    day 18

--- Tests/aocTests/Day18Tests.swift
+++ Tests/aocTests/Day18Tests.swift
@@ -3,12 +3,33 @@ import XCTest
 
 final class Day18Tests: XCTestCase {
     func testDay18_1() throws {
-        let day = Day18(rawInput: "foo")
-        XCTAssertEqual(day.part1(), 0)
+        let program = """
+        set a 1
+        add a 2
+        mul a a
+        mod a 5
+        snd a
+        set a 0
+        rcv a
+        jgz a -1
+        set a 1
+        jgz a -2
+        """
+        let day = Day18(rawInput: program)
+        XCTAssertEqual(day.part1(), 4)
     }
 
     func testDay18_2() throws {
-        let day = Day18(rawInput: "bar")
-        XCTAssertEqual(day.part2(), 0)
+        let program = """
+        snd 1
+        snd 2
+        snd p
+        rcv a
+        rcv b
+        rcv c
+        rcv d
+        """
+        let day = Day18(rawInput: program)
+        XCTAssertEqual(day.part2(), 3)
     }
 }
