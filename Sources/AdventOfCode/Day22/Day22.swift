e55cf83067484f6fd0db48ce65e33dc296f08521
Gereon Steffens <gereon@steffens.org>
2022-07-31 10:32:15 +0200

    day 22

--- Sources/AdventOfCode/Day22/Day22.swift
+++ Sources/AdventOfCode/Day22/Day22.swift
@@ -6,17 +6,120 @@
 
 import AoCTools
 
+enum TurnDirection {
+    case right // clockwise
+    case left // counterclockwise
+}
+
+extension Point.Direction {
+    static let turns: [TurnDirection: [Point.Direction: Point.Direction]] = [
+        .right: [
+            .n: .e,
+            .e: .s,
+            .s: .w,
+            .w: .n,
+        ],
+        .left: [
+            .n: .w,
+            .e: .n,
+            .s: .e,
+            .w: .s
+        ]
+    ]
+    func turn(_ direction: TurnDirection) -> Point.Direction {
+        Self.turns[direction]![self]!
+    }
+}
+
+enum NodeState {
+    case clean
+    case weakened
+    case infected
+    case flagged
+}
+
 final class Day22: AOCDay {
-    let input: String
+    let grid: [Point: NodeState]
     init(rawInput: String? = nil) {
-        self.input = rawInput ?? Self.rawInput
+        let input = rawInput ?? Self.rawInput
+        var grid = [Point: NodeState]()
+        for (y, line) in input.components(separatedBy: "\n").enumerated() {
+            for (x, char) in line.enumerated() {
+                grid[Point(x, y)] = char == "#" ? .infected : .clean
+            }
+        }
+        self.grid = grid
     }
 
     func part1() -> Int {
-        return 0
+        part1(loops: 10_000)
+    }
+
+    func part1(loops: Int) -> Int {
+        let maxX = grid.keys.max { $0.x < $1.x }!.x
+        let maxY = grid.keys.max { $0.y < $1.y }!.y
+        
+        var grid = grid
+        let start = Point(maxX/2, maxY/2)
+        var current = start
+        var direction = Point.Direction.n
+
+        var infections = 0
+
+        for _ in 0..<loops {
+            let turn: TurnDirection
+            if grid[current] == .infected {
+                turn = .right
+                grid[current] = .clean
+            } else {
+                turn = .left
+                grid[current] = .infected
+
+
+                infections += 1
+            }
+
+            direction = direction.turn(turn)
+            current = current + direction.offset
+        }
+
+        return infections
     }
 
     func part2() -> Int {
-        return 0
+        part2(loops: 10_000_000)
+    }
+
+    func part2(loops: Int) -> Int {
+        let maxX = grid.keys.max { $0.x < $1.x }!.x
+        let maxY = grid.keys.max { $0.y < $1.y }!.y
+
+        var grid = grid
+        let start = Point(maxX/2, maxY/2)
+        var current = start
+        var direction = Point.Direction.n
+
+        var infections = 0
+
+        for _ in 0..<loops {
+            switch grid[current] {
+            case .clean, .none:
+                direction = direction.turn(.left)
+                grid[current] = .weakened
+            case .weakened:
+                grid[current] = .infected
+                infections += 1
+            case .infected:
+                direction = direction.turn(.right)
+                grid[current] = .flagged
+            case .flagged:
+                direction = direction.opposite
+                grid[current] = .clean
+            }
+
+            current = current + direction.offset
+        }
+
+        return infections
     }
 }
--- Tests/aocTests/Day22Tests.swift
+++ Tests/aocTests/Day22Tests.swift
@@ -3,12 +3,25 @@ import XCTest
 
 final class Day22Tests: XCTestCase {
     func testDay22_1() throws {
-        let day = Day22(rawInput: "foo")
-        XCTAssertEqual(day.part1(), 0)
+        let grid = """
+        ..#
+        #..
+        ...
+        """
+        let day = Day22(rawInput: grid)
+        XCTAssertEqual(day.part1(loops: 7), 5)
+        XCTAssertEqual(day.part1(loops: 70), 41)
+        XCTAssertEqual(day.part1(), 5587)
     }
 
     func testDay22_2() throws {
-        let day = Day22(rawInput: "bar")
-        XCTAssertEqual(day.part2(), 0)
+        let grid = """
+        ..#
+        #..
+        ...
+        """
+        let day = Day22(rawInput: grid)
+        XCTAssertEqual(day.part2(loops: 100), 26)
+        XCTAssertEqual(day.part2(loops: 10000000), 2511944)
     }
 }
