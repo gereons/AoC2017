f58f57a7c0102b05cef24fe341f0d6432aefbfd0
Gereon Steffens <gereon@steffens.org>
2022-08-06 16:23:09 +0200

    day 21; 50 stars

--- Tests/aocTests/Day21Tests.swift
+++ Tests/aocTests/Day21Tests.swift
@@ -3,12 +3,11 @@ import XCTest
 
 final class Day21Tests: XCTestCase {
     func testDay21_1() throws {
-        let day = Day21(rawInput: "foo")
-        XCTAssertEqual(day.part1(), 0)
-    }
-
-    func testDay21_2() throws {
-        let day = Day21(rawInput: "bar")
-        XCTAssertEqual(day.part2(), 0)
+        let input = """
+        ../.# => ##./#../...
+        .#./..#/### => #..#/..../..../#..#
+        """
+        let day = Day21(rawInput: input)
+        XCTAssertEqual(day.part1(iterations: 2), 12)
     }
 }
